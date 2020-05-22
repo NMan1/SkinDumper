48aa8c13c283d804ea0fa48eb696bc4d009bfcf7
NMan1 <nickmantini01@gmail.com>
2020-05-21 19:46:35 -0500

    Added text file

--- main.py
+++ main.py
@@ -14,24 +14,29 @@ hrefs = driver.find_elements_by_css_selector("ul li a")
 for href in hrefs:
     links.append(href.get_attribute('href'))
 
-for link in links:
-    if link is not None:
-        driver.get(link)
-        title = driver.find_element_by_css_selector(".headline .center").text
-        if title.find("CASE") != -1:
-            break
-        else:
-            weapon_name = title[:title.find("PRICE")]
-            skin_data[weapon_name] = {}
-            time.sleep(.5)
-            print("\n"+weapon_name)
+with open("skins.text", "r+", encoding='utf-8') as file:
+    for link in links:
+        if link is not None:
+            driver.get(link)
+            title = driver.find_element_by_css_selector(".headline .center").text
+            if title.find("CASE") != -1:
+                break
+            else:
+                weapon_name = title[:title.find("PRICE")]
+                skin_data[weapon_name] = {}
+                file.write(f"[{weapon_name}]\n")
+                file.write("{\n")
+                time.sleep(.5)
+                print("\n"+weapon_name)
 
-            weapon_skins = driver.find_elements_by_css_selector(".item-name .name")
-            qualitys = driver.find_elements_by_css_selector(".center.item-rare p")
+                weapon_skins = driver.find_elements_by_css_selector(".item-name .name")
+                qualitys = driver.find_elements_by_css_selector(".center.item-rare p")
 
-            for skins, quality, in zip(weapon_skins, qualitys):
-                print(f'{skins.text[skins.text.lower().find("|")+2:]}, {qualitys_dict[quality.text]}')
-                skin_data[weapon_name][skins.text[skins.text.lower().find("|")+2:]] = qualitys_dict[quality.text]
+                for skins, quality, in zip(weapon_skins, qualitys):
+                    print(f'{skins.text[skins.text.lower().find("|")+2:]}, {qualitys_dict[quality.text]}')
+                    skin_data[weapon_name][skins.text[skins.text.lower().find("|")+2:]] = qualitys_dict[quality.text]
+                    file.write(f"{skins.text[skins.text.lower().find('|')+2:]}:{qualitys_dict[quality.text]}\n")
+                file.write("}\n")
 
 driver.close()
 driver.quit()
